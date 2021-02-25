c440c8bdb729483e2754aebd1012c6eee0309db4
tianaos <zafisainana.randria.o@gmail.com>
2021-02-25 10:32:06 +0100

    commentaires

---
+++ comments.py
@@ -0,0 +1,6 @@
+num = 2
+
+if num%2 == 0:
+    print("It is Even")
+else:
+    print("It is odd")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+"""
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
@@ -15,3 +15,18 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""
+
+
+print('My', 'Name', 'Is', 'Pylenin', sep="|")
+
+
+print("Welcome", end=' ')
+print("to", end=' ')
+print("Python", end=' ')
+print("Bootcamp", end=' ')
+print("2021")
+
+
+print('Hello World', file=open('hello.txt', 'w'))
